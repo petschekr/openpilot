79063a37bcfaa0b317bc128ccd5dcb0614cd1477
Ryan Petschek <petschekr@gmail.com>
2024-11-28 09:31:10 -0500

    Display proof-of-concept off-road BMS data

---
+++ selfdrive/ui/qt/offroad/ioniq.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <QLabel>
+#include <QStackedWidget>
+#include <QVBoxLayout>
+#include <QWidget>
+
+#include "selfdrive/ui/ui.h"
+
+class IoniqWidget : public QFrame {
+  Q_OBJECT
+public:
+  explicit IoniqWidget(QWidget* parent = 0);
+  void updateState(const UIState& s);
+private:
+  QLabel *description;
+};

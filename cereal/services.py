0ebfb9aa04b5b468d0a1af849f104c398369750b
Ryan Petschek <petschekr@gmail.com>
2024-11-28 05:40:49 -0500

    Get HUD data from Rust via ZMQ bridge

--- cereal/services.py
+++ cereal/services.py
@@ -91,6 +91,7 @@ _services: dict[str, tuple] = {
   "customReservedRawData0": (True, 0.),
   "customReservedRawData1": (True, 0.),
   "customReservedRawData2": (True, 0.),
+  "ioniq": (True, 0.),
 }
 SERVICE_LIST = {name: Service(*vals) for
                 idx, (name, vals) in enumerate(_services.items())}

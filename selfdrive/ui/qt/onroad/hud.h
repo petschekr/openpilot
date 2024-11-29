31b206fce5c89bf8432f339027112f164ca41d40
Ryan Petschek <petschekr@gmail.com>
2024-11-29 08:44:32 -0500

    Swap display sides and reset energy counter when charging starts or ends

--- selfdrive/ui/qt/onroad/hud.h
+++ selfdrive/ui/qt/onroad/hud.h
@@ -21,11 +21,14 @@ private:
 
   float speed = 0;
   float set_speed = 0;
+
+  bool shouldEnergyReset = true;
+  cereal::Ioniq::ChargingType lastChargingType = cereal::Ioniq::ChargingType::NOT_CHARGING;
   double altitude = 0;
   float power = 0;
-  bool shouldEnergyReset = true;
   float startEnergy = 0;
   float currentEnergy = 0;
+
   bool is_cruise_set = false;
   bool is_cruise_available = true;
   bool is_metric = false;

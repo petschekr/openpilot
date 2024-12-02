49a3d339eeff109c85a0df43eaea7f7612edc6b2
Ryan Petschek <petschekr@gmail.com>
2024-12-02 09:48:21 -0500

    Display additional metrics in the onroad/offroad UI

--- selfdrive/ui/qt/onroad/hud.h
+++ selfdrive/ui/qt/onroad/hud.h
@@ -17,10 +17,19 @@ private:
   void drawAltitude(QPainter &p, const QRect &surface_rect);
   void drawPower(QPainter &p, const QRect &surface_rect);
   void drawEnergy(QPainter& p, const QRect& surface_rect);
+
+  void drawChargePower(QPainter& p, const QRect& surface_rect);
+  void drawDischargePower(QPainter& p, const QRect& surface_rect);
+  void drawBatteryTemps(QPainter& p, const QRect& surface_rect);
+  void drawOtherTemps(QPainter& p, const QRect& surface_rect);
+  void drawSuntimes(QPainter& p, const QRect& surface_rect);
+
   void drawText(QPainter &p, int x, int y, const QString &text, int alpha = 255);
 
   float speed = 0;
   float set_speed = 0;
+
+  int fullScreenWidth = 0;
 
   bool shouldEnergyReset = true;
   cereal::Ioniq::ChargingType lastChargingType = cereal::Ioniq::ChargingType::NOT_CHARGING;
@@ -28,6 +37,15 @@ private:
   float power = 0;
   float startEnergy = 0;
   float currentEnergy = 0;
+
+  float maxChargePower = 0;
+  float maxDischargePower = 0;
+  int8_t minBatteryTemp = 0;
+  int8_t maxBatteryTemp = 0;
+  int8_t batteryInletTemp = 0;
+  int8_t heaterTemp = 0;
+  std::string sunrise = "--:--";
+  std::string sunset = "--:--";
 
   bool is_cruise_set = false;
   bool is_cruise_available = true;
